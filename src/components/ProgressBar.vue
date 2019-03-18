c565bae8e390b1d7204efe9c1c19577bf5237764
Isaac Omondi <iomondi90@gmail.com>
2019-03-18 17:32:56 +0300

    added progressbar that fetches data dynamically from app.vue

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <h1>Quotes Added</h1>
+
+    <div class="progress" style="height: 25px; margin-bottom:10px;">
+      <div class="progress-bar" :style="{width:quoteCount*10+'%'}">{{quoteCount}}/{{maxQuotes}} </div>
+    </div>
+
+    <div>
+  </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  props: ['quoteCount','maxQuotes']
+};
+</script>
+
+<style scoped>
+    div.header {
+        margin: 2rem 0;
+        width: 100%
+    }
+</style>

8dc2c5b286b773989d44b2eebc7c3e28bba2aff3
Isaac Omondi <iomondi90@gmail.com>
2019-03-18 17:33:29 +0300

    quote component that displays posts once created from addquote component

---
+++ src/components/Quote.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="card">
+      <div class="card-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.card-body {
+  font-family: "ZCOOL XiaoWei", cursive;
+  font-size: 24px;
+  font-weight: 600;
+  color: #6e6e6e;
+}
+
+.quote {
+  cursor: pointer;
+}
+.quote:hover {
+  background-color: #ffe2e2;
+}
+</style>

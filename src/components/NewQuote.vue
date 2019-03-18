49fa05903fcd1faf6404faf5d412979cee8eb740
Isaac Omondi <iomondi90@gmail.com>
2019-03-18 17:32:22 +0300

    logic and ui for creating a new quote

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row">
+    <form style="width:100%">
+      <div class="col-sm-8 offset-sm-2 col-xs-12 col-md-6 offset-md-3 form-group">
+        <label for="quote">Quote</label>
+        <textarea class="form-control" rows="3" v-model="quote" required></textarea>
+      </div>
+
+      <div class="col-sm-8 offset-sm-2 col-xs-12 col-md-6 offset-md-3 form-group">
+        <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      quote: ""
+    };
+  },
+  methods: {
+    createNew() {
+      if (this.quote === "") {
+          alert("Please enter a quote.");
+      } else {
+        this.$emit("quoteAdded", this.quote);
+        this.quote = "";
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
